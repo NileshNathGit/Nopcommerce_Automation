34c99f5c12f97d8e52ed9ad6ccef9f66917f6954
Nath <nilesh.kumar.nath@accenture.com>
2025-06-17 14:07:29 +0530

    2nd commit

---
+++ src/test/java/base/base.java
@@ -0,0 +1,9 @@
+package base;
+
+public class base {
+
+
+	public static final  String url = "https://tutorialsninja.com/demo";
+
+
+}
---
+++ src/test/java/script/launch.java
@@ -0,0 +1,18 @@
+package script;
+
+import base.base;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class launch {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get(base.url);
+//		bjhj
+		
+	}
+		
+	}
+
+
