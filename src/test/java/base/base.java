bd6d0b34fe9b89625e8b8f78476e25b8d329fe45
NileshNathGit <nileshnath1706@gmail.com>
2025-06-30 21:42:29 +0530

    Added log

--- src/test/java/base/base.java
+++ src/test/java/base/base.java
@@ -2,21 +2,29 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class base {
+	
 	WebDriver driver;
-
+	public static  Logger logger = LogManager.getLogger();
 	public static final  String url = "https://tutorialsninja.com/demo";
 	public static WebDriver launch(WebDriver driver)
 	{
+		
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(base.url);
 		return driver;
 	}
+	
+	
+		
+		
 
 	public static final String _report = System.getProperty("user.dir")+"/reports/Automation_Report.html";
 
--- src/test/java/script/Register.java
+++ src/test/java/script/Register.java
@@ -1,32 +1,42 @@
 package script;
 
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import base.base;
 import methods.RegisterMethods;
+import utilities.MyListener;
 
+@Listeners(MyListener.class)
 public class Register {
 	RegisterMethods rm = new RegisterMethods();
+	Logger logger;
 	WebDriver driver;
+	
+	
 	@BeforeClass
 	public void launch()
 	{
 		driver = base.launch(driver);
+		base.logger.info("*****  Browser Launch  ****");
+		
 	}
 
 	@Test
 	public void register()
 	{
-		
+		base.logger.info("****************** Register fucntion Start ______________");
 		rm.register(driver,"Nilesh", "Nath", "email@xcd.com","145245245", "Masspower@rfsan123");
 		
 	}
 	@AfterClass
 	public void tearDown()
 	{
+		base.logger.info("*************** Driver close *******************");
 		driver.close();
 	}
 }
